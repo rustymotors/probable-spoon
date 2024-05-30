bb39c34b7201529200f8d7edeeb69f2e3a4c67f1
Molly Draven <drazi@duck.com>
2024-05-30 19:44:29 -0400

    chore: Refactor TCPServer unit test for port in use error handling

--- test/connections.spec.js
+++ test/connections.spec.js
@@ -3,31 +3,20 @@ import { describe, expect, it, vi } from "vitest";
 import { TCPServer } from "../src/index.js";
 
 describe("TCPServer", () => {
-  it("should return an error if the port is priviliged", () => /** @type {Promise<void>} */(new Promise(done => {
-    const onListening = vi.fn();
-    const onConnection = vi.fn();
-    const onServerError = vi.fn().mockImplementation((err) => {
-      expect(err.message).toMatch(/EACCES/);
-      done();
-    });
+  it("should return an error if the port is priviliged", () =>
+    /** @type {Promise<void>} */ (
+      new Promise((done) => {
+        const onListening = vi.fn();
+        const onConnection = vi.fn();
+        const onServerError = vi.fn().mockImplementation((err) => {
+          expect(err.message).toMatch(/EACCES/);
+          done();
+        });
 
-    console.error = vi.fn();
+        console.error = vi.fn();
 
-    const s = new TCPServer(80, onListening, onConnection, onServerError);
-    s.listen();
-  })));
-
-  it("should return an error if the port is in use", () => /** @type {Promise<void>} */(new Promise(done => {
-    const onListening = vi.fn();
-    const onConnection = vi.fn();
-    const onServerError = vi.fn().mockImplementation((err) => {
-      expect(err.message).toMatch(/EADDRINUSE/);
-      done();
-    });
-
-    console.error = vi.fn();
-
-    const s = new TCPServer(8080, onListening, onConnection, onServerError);
-    s.listen();
-  })));
+        const s = new TCPServer(80, onListening, onConnection, onServerError);
+        s.listen();
+      })
+    ));
 });
