3f874e41495736f55c9c3ca413310aa464833e5a
Molly Draven <drazi@duck.com>
2024-05-30 17:58:50 -0400

    chore: Update TCPServer unit test for port in use error handling

--- test/connections.spec.js
+++ test/connections.spec.js
@@ -3,7 +3,7 @@ import { describe, expect, it, vi } from "vitest";
 import { TCPServer } from "../src/index.js";
 
 describe("TCPServer", () => {
-  it("should return an error if the port is in use", () => /** @type {Promise<void>} */(new Promise(done => {
+  it("should return an error if the port is priviliged", () => /** @type {Promise<void>} */(new Promise(done => {
     const onListening = vi.fn();
     const onConnection = vi.fn();
     const onServerError = vi.fn().mockImplementation((err) => {
@@ -14,6 +14,20 @@ describe("TCPServer", () => {
     console.error = vi.fn();
 
     const s = new TCPServer(80, onListening, onConnection, onServerError);
+    s.listen();
+  })));
+
+  it("should return an error if the port is in use", () => /** @type {Promise<void>} */(new Promise(done => {
+    const onListening = vi.fn();
+    const onConnection = vi.fn();
+    const onServerError = vi.fn().mockImplementation((err) => {
+      expect(err.message).toMatch(/EADDRINUSE/);
+      done();
+    });
+
+    console.error = vi.fn();
+
+    const s = new TCPServer(8080, onListening, onConnection, onServerError);
     s.listen();
   })));
 });
