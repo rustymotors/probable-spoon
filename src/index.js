6bd9122e8fdfc0ff7d36e03cbec3a2b40659b4d9
Molly Draven <drazi@duck.com>
2024-05-29 20:18:42 -0400

    chore: Add start script to package.json and create TCP server in index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,81 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import net from "node:net";
+
+/**
+ * @typedef listeningHandler
+ * @type {function(net.Server): void}
+ */
+
+/** @typedef connectionHandler
+ * @type {function(net.Socket): void}
+ */
+
+/**
+ *
+ * @param {Buffer} data
+ */
+function onData(data) {
+  const hex = data.toString("hex");
+  console.log(`Data received: ${hex}`);
+}
+
+/**
+ * @param {net.Socket} socket
+ */
+function onConnection(socket) {
+  console.log("Connection established");
+  socket.on("data", onData);
+}
+
+/**
+ *
+ * @param {Error} err
+ */
+function onServerError(err) {
+  console.error(`Server error: ${err.message}`);
+}
+
+/**
+ * @param {net.Server} s
+ */
+function onListening(s) {
+  const address = s.address();
+
+  if (address === null || typeof address === "string") {
+    console.error("Server listening on unknown address");
+    return;
+  }
+
+  const port = address.port;
+
+  console.log(`Server listening on port ${port}`);
+  s.on("connection", onConnection);
+  s.on("close", () => {
+    console.log(`Server on port ${port} closed`);
+  });
+  s.on("error", (err) => {
+    console.error(`Server on port ${port} errored: ${err.message}`);
+  });
+}
+
+/**
+ *
+ * @param {number} port
+ * @param {listeningHandler} onListening
+ * @param {connectionHandler} onConnection
+ */
+function makeTCPServerforPort(port, onListening, onConnection) {
+  const server = net.createServer(onConnection);
+
+  server.on("error", onServerError);
+  server.on("listening", onListening);
+  server.listen(port);
+  return server;
+}
+
 export default function main() {
   console.log("Hello, world!");
+  makeTCPServerforPort(8080, onListening, onConnection);
 }
