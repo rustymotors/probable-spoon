a2d9deb08ad4e20cb9e42082bab4ead3eda766ca
Molly Draven <drazi@duck.com>
2024-06-01 17:24:30 -0400

    refactor: Add NPS data handling and web request handling functions

---
+++ packages/main/src/NPSMessage.js
@@ -0,0 +1,42 @@
+import { NPSMessageHeader } from "./NPSMessageHeader.js";
+import { NPSMessagePayload } from "./NPSMessagePayload.js";
+
+export class NPSMessage {
+  constructor() {
+    this._header = new NPSMessageHeader();
+    this.data = new NPSMessagePayload();
+  }
+  /**
+   *
+   * @param {Buffer} data
+   * @returns {NPSMessage}
+   */
+  static parse(data) {
+    const self = new NPSMessage();
+    if (data.length < 8) {
+      throw new Error(`Invalid message length: ${data.length}`);
+    }
+
+    self._header = NPSMessageHeader.parse(data);
+
+    const expectedLength = self._header.messageLength - self._header.dataOffset;
+
+    self.data = NPSMessagePayload.parse(data.subarray(self._header.dataOffset), expectedLength);
+
+    return self;
+  }
+
+  /**
+   * @returns Buffer
+   */
+  toBuffer() {
+    return Buffer.concat([this._header.toBuffer(), this.data.toBuffer()]);
+  }
+
+  /**
+   * @returns string
+   */
+  toString() {
+    return `${this._header.toString()}, Data: ${this.data.toString()}`;
+  }
+}
